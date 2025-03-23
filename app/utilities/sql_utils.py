ec993c4d9bc4376f705c22debdca8c8a8ff56cb8
Midnite <pmidnite@email.com>
2025-03-23 20:25:50 +0530

    Hide secure fields.

--- app/utilities/sql_utils.py
+++ app/utilities/sql_utils.py
@@ -1,4 +1,5 @@
 from app.utilities.database import db
 
 def map_class_to_dict(class_table):
-    return {key.title().replace('_', ' '): value for key, value in class_table.__dict__.items() if not key.startswith('_')}
+    skip_fields = ["_sa_instance_state", "id", "email", "reviewed"]
+    return {key.title().replace('_', ' '): value for key, value in class_table.__dict__.items() if key not in skip_fields}
