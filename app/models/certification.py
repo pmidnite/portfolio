dcba152c4677edc1de2465395c5912eaf3a3b9da
Midnite <pmidnite@email.com>
2025-08-10 21:18:58 +0530

    Refactored certification model and API.

--- app/models/certification.py
+++ app/models/certification.py
@@ -16,3 +16,63 @@ class Certification(db.Model):
     cert_logo = db.Column(db.String(450), nullable=False)
     cert_url = db.Column(db.String(450), nullable=True)
     cert_type = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Certification {self.cert_name}>"
+
+    @classmethod
+    def fetch_all_records(cls):
+        """
+        Get the first record from the Certification table.
+        :return: The first Certification instance if found, else None.
+        """
+        return cls.query.all()
+
+    @classmethod
+    def fetch_all_records_dict(cls):
+        """
+        Get the all record from the Certification table as a dictionary.
+        :return: Dictionary representation of the all Certification instance if found, else None.
+        """
+        all_record = cls.query.all()
+        return [record.to_dict() for record in all_record if record]
+
+    @classmethod
+    def filter_by_cert_name(cls, cert_name):
+        """
+        Filter records by cert_name.
+        :param cert_name: Cert Name to filter by
+        :return: The About instance if found, else None.
+        """
+        return cls.query.filter_by(cert_name=cert_name).first()
+
+    def save(self):
+        """
+        Save the current instance to database.
+        """
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+
+    def delete(self):
+        """
+        Delete the current instance from database.
+        """
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except:
+            db.session.rollback()
+
+    def to_dict(self):
+        """
+        Convert the Certification instance to a dictionary.
+        """
+        return {
+            "Cert Name": self.cert_name,
+            "Cert Logo": self.cert_logo,
+            "Cert Url": self.cert_url,
+            "Cert Type": self.cert_type
+        }
--- app/routes/certification.py
+++ app/routes/certification.py
@@ -1,50 +1,51 @@
 from flask import Blueprint, request, jsonify
+from flask.views import MethodView
+from flask_smorest import Blueprint
 from flask_jwt_extended import jwt_required
-from bson import json_util
 from app.utilities.utils import *
 from app.models.certification import Certification
 
-bp = Blueprint('certification', __name__, url_prefix='/api/certification')
+bp = Blueprint('certification', __name__, description='Certification Implementation Logic', url_prefix='/api')
 
-@bp.route('', methods=['GET'])
-def fetch_certification():
-    certs = Certification.query.all()
-    if certs:
-        return json_util.dumps(map_class_to_dict(cert) for cert in certs)
-    else:
+
+@bp.route('/certification')
+class CertificationView(MethodView):
+    def get(self):
+        certs = Certification.fetch_all_records_dict()
+        if certs:
+            return certs
         return jsonify({"Message": "No certification exists."})
 
-@bp.route('', methods=['POST', 'PATCH'])
-@jwt_required()
-def insert_or_update_certification():
-    certification_payload = request.json
-    is_exists = Certification.query.filter_by(cert_name=certification_payload.get('Cert Name')).first()
-    unavailable_column = []
+    @jwt_required()
+    def post(self):
+        certification_payload = request.get_json()
+        is_exists = Certification.filter_by_cert_name(cert_name=certification_payload.get('Cert Name'))
+        unavailable_column = []
 
-    final_obj = is_exists or Certification()
-    for key, value in certification_payload.items():
-        if hasattr(final_obj, key.lower().replace(' ', '_')):
-            setattr(final_obj, key.lower().replace(' ', '_'), value)
-        else:
-            unavailable_column.append(key)
-    db.session.add(final_obj)
-    db.session.commit()
-    if unavailable_column:
-        message = " except these values {0} as these column doesn't exists".format(unavailable_column)
-        return jsonify({"Message": "Certification inserted/updated successfully" + message})
-    return jsonify({'Message': 'Certification inserted/updated successfully'})
+        final_obj = is_exists or Certification()
+        for key, value in certification_payload.items():
+            if hasattr(final_obj, key.lower().replace(' ', '_')):
+                setattr(final_obj, key.lower().replace(' ', '_'), value)
+            else:
+                unavailable_column.append(key)
 
-@bp.route('', methods=['DELETE'])
-@jwt_required()
-def delete_certification():
-    try:
-        del_payload = request.json
-        is_exists = Certification.query.filter_by(cert_name=del_payload.get('Cert Name')).first()
-        if is_exists:
-            db.session.delete(is_exists)
-            db.session.commit()
-            return jsonify({'Message': 'Certification deleted successfully'})
-        else:
-            return jsonify({'Message': "Cert Name: {0} doesn't exists.".format(del_payload.get('Cert Name'))})
-    except:
-        return jsonify({"Message": "Some exception occured."})
+        final_obj.save()
+
+        if unavailable_column:
+            message = " except these values {0} as these column doesn't exists".format(unavailable_column)
+            return jsonify({"Message": "Certification inserted/updated successfully" + message})
+
+        return jsonify({'Message': 'Certification inserted/updated successfully'})
+
+    @jwt_required()
+    def delete(self):
+        try:
+            del_payload = request.get_json()
+            is_exists = Certification.filter_by_cert_name(cert_name=del_payload.get('Cert Name'))
+            if is_exists:
+                is_exists.delete()
+                return jsonify({'Message': 'Certification deleted successfully'})
+            else:
+                return jsonify({'Message': "Cert Name: {0} doesn't exists.".format(del_payload.get('Cert Name'))})
+        except Exception as e:
+            return jsonify({"Message": "Some exception occurred: {}".format(str(e))})
